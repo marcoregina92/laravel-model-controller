c92e2d459af6338e4485df59169e9dd27463ee50
Marco <marcoregina92@gmail.com>
2024-01-22 19:24:35 +0100

    stampa cards e completa esercizio + bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,24 @@
 @section('content')
 <h1>Film</h1>
 
-<ul>
-@foreach ($films as $film)
-    <li>{{$film->title}}</li>
-@endforeach
+<div class="container">
+    <div class="row">
+        <div class="d-flex flex-wrap">
+            @foreach ($films as $film)
+                <div class="card m-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h2 class="card-title">{{$film->title}}</h2>
+                        <h5 class="card-text">Nazionalità: {{$film->nationality}}</h5>
+                        <h5 class="card-text">Data di uscita: {{$film->date}}</h5>
+                        <h5 class="card-text">Voto: {{$film->vote}}</h5>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
 </ul>
+
 
 @endsection
